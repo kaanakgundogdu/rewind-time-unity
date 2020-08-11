0b992146b91cf5980ec245776787e33e14f1f8ce
Kaan Akgundogdu <kaanakgundogdu@gmail.com>
2020-08-11 18:51:09 +0300

    Rewind

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Vector3 _offset;
+    private float smoothTime = 0.9F;
+    [SerializeField] private GameObject _followMe;
+
+
+    void Start()
+    {
+       _offset = transform.position - _followMe.transform.position;
+
+    }
+
+    void LateUpdate()
+    {
+        transform.position = _followMe.transform.position + _offset;
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    private float _speed = 5f;
+    private float _horizontalInput;
+    private float _verticalInput;
+    private float _jumpForce = 5f;
+    [SerializeField] private bool _isOnGround = false;
+    private Vector3 _position;
+    private Vector3 _inputVector;
+    private Rigidbody _myRigidBody;
+    
+
+    void Start()
+    {
+        _myRigidBody = GetComponent<Rigidbody>();
+        
+    }
+
+	void Update()
+	{
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+
+
+        _inputVector = new Vector3( _horizontalInput *_speed , _myRigidBody.velocity.y , _verticalInput *_speed );
+
+        transform.LookAt( transform.position + new Vector3 (_inputVector.x, 0, _inputVector.z) );
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isOnGround  )
+        {
+            _myRigidBody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            _isOnGround = false;
+        }
+    }
+
+	private void FixedUpdate()
+	{
+
+        _myRigidBody.velocity = _inputVector;
+	}
+
+	private void OnCollisionEnter (Collision collision)
+	{
+		if( collision.gameObject.tag == ("Ground"))
+		{
+            _isOnGround = true;
+		}
+	}
+
+
+}
